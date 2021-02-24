de974caa3d97624f4e159bb692a7d6452e965644
Sean <skabe89@gmail.com>
2021-02-23 23:12:06 -0500

    commit tree a9c245483262ed2c961b525b43a1c0b7390b1235

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+
+function saturdayFun(activity = "roller-skate"){
+  return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = "go to the office"){
+  return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(symbol="*"){
+  return function(adj){
+    return `You are ${symbol}${adj}${symbol}!`
+  }
+}
+
+let Calculator = {
+  add: function(a, b){return a + b},
+  subtract: function(a, b){return a - b},
+  multiply: function(a, b){return a * b},
+  divide: function(a, b){return a / b},
+}
+
+actionApplyer = function(int, array){
+  array.forEach(e => {
+    int = e(int)
+  })
+  return int
+}
